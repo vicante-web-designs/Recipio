25df73cc89f98a8b8a058bf65698203eb03c8465
Batubo Victory <victorybatubo76@gmail.com>
2026-02-02 17:56:44 +0100

    feat: import RecipesGallery and ad content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import RecipesGallery from './Layout/RecipesGallery'
 
 function App() {
   return (
     <>
-      
+      <h1 className='text-center font-bold text-5xl'>Best place to get fresh recipes</h1>
+      <RecipesGallery />
     </>
   )
 }
