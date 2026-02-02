d1a59067d0be0329ff70cb921daee9d85f7bada2
Batubo Victory <victorybatubo76@gmail.com>
2026-02-02 19:13:10 +0100

    feat: Add onClick function type

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,12 @@
 interface ButtonProps{
     type: 'button' | 'submit' | 'reset';
-    content: string
+    content: string;
+    onClick: () => Promise<void>;
 }
 
-function Button({type, content}: ButtonProps){
+function Button({type, content, onClick}: ButtonProps){
     return(
-        <button type={type} className='bg-blue-700 text-white px-4 py-2 rounded-full'>
+        <button type={type} className='bg-blue-900 text-white px-4 py-2 rounded-full active:bg-blue-700 hover:bg-blue-500' onClick={() => {onClick()}}>
             {content}
         </button>
     )
