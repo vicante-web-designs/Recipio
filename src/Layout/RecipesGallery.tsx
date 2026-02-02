2edb1d4fdbc24ba14ae9a96744dbd41068cbd763
Batubo Victory <victorybatubo76@gmail.com>
2026-02-02 17:57:05 +0100

    feat: Import button

--- src/Layout/RecipesGallery.tsx
+++ src/Layout/RecipesGallery.tsx
@@ -1,8 +1,10 @@
+import Button from '../components/Button';
+
 function RecipesGallery() {
   return(
-    <>
-    
-    </>
+    <section className='mx-auto my-4 w-fit'>
+        <Button type='button' content='Get recipes now' />
+    </section>
   )
 }   
 
